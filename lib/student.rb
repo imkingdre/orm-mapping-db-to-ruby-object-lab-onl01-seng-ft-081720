6c51652e776315439d7fce3f5ca13647ce426331
Andre King <andreking1031@gmail.com>
2020-09-28 18:42:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,33 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+
+    s = DB[:conn].execute(sql)
+    s.map do |i|
+      self.new_from_db(i)
+    end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -39,5 +55,44 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+       SELECT COUNT(grade = 9) FROM students
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+       SELECT COUNT(grade > 10) FROM students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+       SELECT * FROM students WHERE grade = 10 LIMIT ?
+    SQL
+
+    s = DB[:conn].execute(sql, x)
+    # binding.pry
+  end
+
+  def self.first_student_in_grade_10
+    s = self.first_X_students_in_grade_10(1)
+    s.map do |i|
+      self.new_from_db(i)
+    end[0]
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+       SELECT * FROM students WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, grade)
   end
 end
